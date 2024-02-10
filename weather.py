63f00e8ea7ae06a0e8e64f21e2efa7b4de6ad28e
FOMOlabauother <158472327+FOMOlabauother@users.noreply.github.com>
2024-02-10 22:43:19 +1100

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+from loguru import logger
 import requests
 
 def get_weather(api_key, city):
@@ -13,7 +14,22 @@ def get_weather(api_key, city):
     else:
         return None
 
-if __name__ == "__main__":
+def setup_logging():
+    logger_format = (
+        "<green>{time:HH:mm:ss}</green> | "
+        "<level>{message}</level>"
+    )
+    logger.configure() # Default values
+    logger.remove()
+    logger.add(sys.stderr, format=logger_format)
+    
+    log_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "logs") # Get directory of where this script is running 
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+    log_file = os.path.join(log_folder, "weather-{time:YYYYMMDD}.log")
+    logger.add(log_file, rotation="100 MB", format=logger_format)    
+    
+if __name__ == "__main__":   
     api_key = '<YOUR_API_KEY_HERE>'  # Replace this with your actual API key
     city = 'Melbourne'  # Example city
     weather = get_weather(api_key, city)
